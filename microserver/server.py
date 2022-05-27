8be446b815a3f35a2a2c3ca179386e3e5f4242eb
Roberto Ulloa <roberto.ur@gmail.com>
2022-05-27 21:01:22 +0200

    update microserver support for python3

--- microserver/server.py
+++ microserver/server.py
@@ -1,4 +1,12 @@
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import sys
+version = sys.version_info[0]
+
+# control for python version
+if version == 2:
+    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+elif version == 3:
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+
 import simplejson
 
 class S(BaseHTTPRequestHandler):
@@ -11,13 +19,19 @@ class S(BaseHTTPRequestHandler):
         if self.path == "/bot/nextround":
             self._set_headers()
             f = open("bot/nextround.html", "r")
-            self.wfile.write(f.read())
+            if version == 2:
+               self.wfile.write(f.read())
+            elif version == 3:
+               self.wfile.write(bytes(f.read(), "utf-8"))
 
     def do_HEAD(self):
         self._set_headers()
 
     def do_POST(self):
-        content_len = int(self.headers.getheader('content-length'))
+        if version == 2:
+            content_len = int(self.headers.getheader('content-length'))
+        elif version == 3:
+            content_len = int(self.headers.get('content-length'))
         post_body = self.rfile.read(content_len)
         data = simplejson.loads(post_body)
         self.send_response(200)
@@ -43,7 +57,7 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=80):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd...'
+    print ('Starting httpd...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
